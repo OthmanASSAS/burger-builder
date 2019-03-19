a25b81034e47135edc1573480960f4caa8cc9fc8
Othman ASSAS <o.assas@gop.link>
2019-03-19 17:40:11 +0100

    prop-type validation

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const Burger = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = props => {
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="meat" />
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
 };
 
-export default Burger;
+export default burger;

6b04c92e4359f659192b84de56ebc40fd7ba4bbd
Othman ASSAS <o.assas@gop.link>
2019-03-20 10:59:23 +0100

    BuildControls component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -15,7 +15,7 @@ class BurgerIngredient extends Component {
         ingredient = (
           <div className={classes.BreadTop}>
             <div className={classes.Seeds1} />
-            <div className={classes.Seeds1} />
+            <div className={classes.Seeds2} />
           </div>
         );
         break;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
-import Burger from '../../components/Burger/Burger'
+import React, { Component } from "react";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 class BurgerBuilder extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <Burger/>
-                <div>Build Controls</div>
-            </React.Fragment>
-        );
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon:0,
+      cheese: 0,
+      meat: 0
     }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Burger ingredients={this.state.ingredients}/>
+        <BuildControls/>
+      </React.Fragment>
+    );
+  }
 }
 
 export default BurgerBuilder;

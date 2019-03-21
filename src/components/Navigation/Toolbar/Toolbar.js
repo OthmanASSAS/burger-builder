c92c1cd2bd0695fcdfbb4f1a97d7aef70f8fff1b
Othman ASSAS <o.assas@gop.link>
2019-03-21 11:15:29 +0100

    responsive adjustements

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Toolbar.module.css';
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems'
+import classes from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = () => (
-        <header className={classes.Toolbar}>
-            <div>MENU</div>
-            <Logo/>
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </header>
-    );
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav>
+      <NavigationItems />
+    </nav>
+  </header>
+);
 
 export default toolbar;

35b3fdc782c11eb9f33cd766ff1c57452a71f4fe
Othman ASSAS <o.assas@gop.link>
2019-03-21 13:39:43 +0100

    Backdrop with SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import classes from './Layout.module.css';
+import React, { Component } from "react";
+import classes from "./Layout.module.css";
 
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
 
-const layout = props => (
-    <React.Fragment>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </React.Fragment>
-)
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </React.Fragment>
+    );
+  }
+}
 
-export default layout;
+export default Layout;

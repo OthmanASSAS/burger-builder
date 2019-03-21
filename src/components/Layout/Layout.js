c71efd8115449f45446dceb712eb24f1975c73d2
Othman ASSAS <o.assas@gop.link>
2019-03-21 14:24:08 +0100

    adding a Hamburger Icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -16,7 +16,7 @@ class Layout extends Component {
   sideDrawerToggleHandler = () => {
       
     this.setState(prevState => {
-        console.log(prevState)
+       
       return { showSideDrawer: !prevState.showSideDrawer };
     });
   };

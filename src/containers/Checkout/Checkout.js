a03c83e4572e4ea0d58d8daec13026ea9d6c4bf5
Othman ASSAS <o.assas@gop.link>
2019-04-01 11:43:37 +0200

    navigating back and to next page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 class Checkout extends Component {
-    state= {
+    state = {
         ingredients: {
-            salad:1,
-            meat:1,
-            cheese:1,
-            bacon:1
+            salad: 1,
+            meat: 1,
+            cheese: 1,
+            bacon: 1
         }
-    }
+    };
+
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    };
+
+    checkoutContinued = () => {
+        this.props.history.replace("/checkout/contact-data");
+    };
     render() {
+        console.log(this.props)
         return (
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients}/>
+                <CheckoutSummary
+                    ingredients={this.state.ingredients}
+                    checkoutCancelled={this.checkoutCancelledHandler}
+                    checkoutContinued={this.checkoutContinued}
+                />
             </div>
         );
     }
